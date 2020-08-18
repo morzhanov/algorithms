79c06a396645da86e3a425fd014b2cdf7e2454fb
Vlad <vlad@Vlads-MacBook-Pro.local>
2020-08-18 14:14:19 +0300

    Added algorithms enable/disable feature

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"local/algorithms/src/ds"
 	"local/algorithms/src/search"
 	"local/algorithms/src/sort"
 	"math/rand"
@@ -20,96 +21,139 @@ func prepareValuesForSeach(performSorting bool) ([]int, int) {
 	return values, value
 }
 
-func prepareValuesForSort() []int {
-	values := make([]int, 1000)
+func prepareValuesForSort(count int) []int {
+	values := make([]int, count)
 	for i := range values {
 		values[i] = rand.Intn(100)
 	}
 	return values
 }
 
+func dataStructures(flags map[string]bool) {
+	if flags["stack"] == true {
+		// Stack
+		var stack ds.Stack
+		ds.TestStack(stack)
+	}
+	if flags["queue"] == true {
+		// Queue
+		var queue ds.Queue
+		ds.TestQueue(queue)
+	}
+	if flags["dynamicArray"] == true {
+		// DynamicArray
+		var da ds.DynamicArray
+		ds.TestDynamicArray(da)
+	}
+	if flags["linkedList"] == true {
+		// LinkedList
+		var l ds.LinkedList
+		ds.TestLinkedList(l)
+	}
+	if flags["doublyLinkedList"] == true {
+		// DoublyLinkedList
+		var dl ds.DoublyLinkedList
+		ds.TestDoublyLinkedList(dl)
+	}
+	if flags["set"] == true {
+		// Set
+		var s ds.Set
+		ds.TestSet(s)
+	}
+	if flags["hashTable"] == true {
+		// HashTable
+		ht := ds.NewHashTable()
+		ds.TestHashTable(ht)
+	}
+	if flags["tree"] == true {
+		// Tree
+		var tn ds.TreeNode
+		ds.TestTree(tn)
+	}
+}
+
+func sorting(flags map[string]bool) {
+	if flags["stupid"] == true {
+		// StupidSorting
+		// extremely stupid sorting,
+		// do not provide more than 5-10 elements or app could stuck
+		values := prepareValuesForSort(5)
+		sort.TestStupidSort(values[:])
+	}
+	if flags["bubble"] == true {
+		// BubbleSorting
+		values := prepareValuesForSort(100)
+		sort.TestBubbleSort(values[:])
+	}
+	if flags["counting"] == true {
+		// CountingSorting
+		values := prepareValuesForSort(100)
+		sort.TestCountingSort(values[:])
+	}
+	if flags["selection"] == true {
+		// SelectionSorting
+		values := prepareValuesForSort(100)
+		sort.TestSelectionSort(values[:])
+	}
+	if flags["merge"] == true {
+		// MergeSorting
+		values := prepareValuesForSort(100)
+		sort.TestMergeSort(values[:])
+	}
+	if flags["insert"] == true {
+		// InsertionSorting
+		values := prepareValuesForSort(100)
+		sort.TestInsertionSort(values[:])
+	}
+}
+
+func searching(flags map[string]bool) {
+	if flags["binary"] == true {
+		// BinarySearch
+		// NOTE: array should be sorted
+		values, value := prepareValuesForSeach(true)
+		search.TestBinarySearch(values[:], value)
+	}
+	if flags["linear"] == true {
+		// LinearSearch
+		values, value := prepareValuesForSeach(false)
+		search.TestLinearSearch(values[:], value)
+	}
+	if flags["jump"] == true {
+		// JumpSearch
+		// NOTE: array should be sorted
+		values, value := prepareValuesForSeach(false)
+		search.TestJumpSearch(values[:], value)
+	}
+}
+
 func main() {
+	// Use those maps to enable/disable algorithms
+	dataStructuresFlags := map[string]bool{
+		"stack":            false,
+		"queue":            false,
+		"dynamicArray":     false,
+		"linkedList":       false,
+		"doublyLinkedList": false,
+		"set":              false,
+		"hashTable":        false,
+		"tree":             false,
+	}
+	sortingFlags := map[string]bool{
+		"stupid":    false,
+		"bubble":    false,
+		"counting":  false,
+		"selection": false,
+		"merge":     false,
+		"insert":    false,
+	}
+	searchingFlags := map[string]bool{
+		"binary": false,
+		"linear": false,
+		"jump":   false,
+	}
 
-	/*
-	* Data Structures
-	 */
-
-	// Stack
-	// var stack ds.Stack
-	// ds.TestStack(stack)
-
-	// Queue
-	// var queue ds.Queue
-	// ds.TestQueue(queue)
-
-	// DynamicArray
-	// var da ds.DynamicArray
-	// ds.TestDynamicArray(da)
-
-	// LinkedList
-	// var l ds.LinkedList
-	// ds.TestLinkedList(l)
-
-	// DoublyLinkedList
-	// var dl ds.DoublyLinkedList
-	// ds.TestDoublyLinkedList(dl)
-
-	// Set
-	// var s ds.Set
-	// ds.TestSet(s)
-
-	// Set
-	// ht := ds.NewHashTable()
-	// ds.TestHashTable(ht)
-
-	// Tree
-	// var tn ds.TreeNode
-	// ds.TestTree(tn)
-
-	/*
-	* Sorting
-	 */
-
-	// StupidSorting
-	// extremely stupid sorting, do not provide more than 5-10 elements or app could stuck
-	// values := prepareValuesForSort()
-	// sort.TestStupidSort(values[:])
-
-	// BubbleSorting
-	// values := prepareValuesForSort()
-	// sort.TestBubbleSort(values[:])
-
-	// CountingSorting
-	// values := prepareValuesForSort()
-	// sort.TestCountingSort(values[:])
-
-	// SelectionSorting
-	// values := prepareValuesForSort()
-	// sort.TestSelectionSort(values[:])
-
-	// MergeSorting
-	// values := prepareValuesForSort()
-	// sort.TestMergeSort(values[:])
-
-	// InsertionSorting
-	// values := prepareValuesForSort()
-	// sort.TestInsertionSort(values[:])
-
-	/*
-	* Search
-	 */
-
-	// BinarySearch
-	// NOTE: array should be sorted
-	// values, value := prepareValuesForSeach(true)
-	// search.TestBinarySearch(values[:], value)
-
-	// BinarySearch
-	// values, value := prepareValuesForSeach(false)
-	// search.TestLinearSearch(values[:], value)
-
-	// JumpSearch
-	// NOTE: array should be sorted
-	values, value := prepareValuesForSeach(false)
-	search.TestJumpSearch(values[:], value)
+	dataStructures(dataStructuresFlags)
+	sorting(sortingFlags)
+	searching(searchingFlags)
 }
