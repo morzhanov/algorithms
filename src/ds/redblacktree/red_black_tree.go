a0f9a4a2823984f00fe1c0e99bbf56ba419c4192
Vlad Morzhanov <vlad.morzhanov@gmail.com>
2020-08-30 19:46:53 +0300

    finished red black tree

--- src/ds/redblacktree/red_black_tree.go
+++ src/ds/redblacktree/red_black_tree.go
@@ -13,6 +13,7 @@ import (
 // 2) Root of tree is always black.
 // 3) There are no two adjacent red nodes (A red node cannot have a red parent or red child).
 // 4) Every path from a node (including root) to any of its descendant NULL node has the same number of black nodes.
+// https://algorithmtutor.com/Data-Structures/Tree/Red-Black-Trees/
 type RedBlackTree struct {
 	root      *Node
 	cursor    *Node
---
+++ src/ds/tree234/2_3_4_tree.go
@@ -0,0 +1,4 @@
+package tree234
+
+// Tree234 is an 2-3-4 Tree implementation
+type Tree234 struct{}
